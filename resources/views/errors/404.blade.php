bf2291ea88fabd9f8d98d17541119f70ca31175a
Ariffudin <ariffudinnotresponding@gmail.com>
2021-04-01 00:05:26 +0700

    Add Custom 404 Error Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error 404</title>
+    <link href="{{ URL::asset('message/style.css') }}" rel="stylesheet">
+</head>
+<body>
+
+ <div id="clouds">
+    <div class="cloud x1"></div>
+    <div class="cloud x1_5"></div>
+    <div class="cloud x2"></div>
+    <div class="cloud x3"></div>
+    <div class="cloud x4"></div>
+    <div class="cloud x5"></div>
+</div>
+<div class='c'>
+    <div class='_404'>404</div>
+    <hr>
+    <div class='_1'>HALAMAN</div>
+    <div class='_2'>TIDAK ADA</div>
+    <a class='btn' href="javascript:history.back()">KEMBALI</a>
+</div>
+
+</body>
+</html>
